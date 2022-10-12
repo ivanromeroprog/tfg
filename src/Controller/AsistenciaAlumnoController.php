c8bb51fdd35a864112c4a3497a29d92cdfc3141a
Iván Yamil Romero <31389316+knewlife@users.noreply.github.com>
2022-10-12 10:26:11 -0300

    Toma de asistencia funciona con alumnos de forma estatica, falta hacer que funcione con ajax y docentes

--- src/Controller/AsistenciaAlumnoController.php
+++ src/Controller/AsistenciaAlumnoController.php
@@ -2,23 +2,27 @@
 
 namespace App\Controller;
 
+use function dump;
+use App\Entity\Asistencia;
 use App\Entity\TomaDeAsistencia;
 use App\Repository\AlumnoRepository;
-use App\Repository\TomaDeAsistenciaRepository;
+use App\Repository\AsistenciaRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use App\Repository\TomaDeAsistenciaRepository;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
-use function dump;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class AsistenciaAlumnoController extends AbstractController
 {
 
     private EntityManagerInterface $em;
     private TomaDeAsistenciaRepository $cr;
+    private AsistenciaRepository $ar;
+
 
     private Session $session;
 
@@ -26,35 +30,61 @@ class AsistenciaAlumnoController extends AbstractController
     {
         $this->em = $em;
         $this->cr = $this->em->getRepository(TomaDeAsistencia::class);
+        $this->ar = $this->em->getRepository(Asistencia::class);
     }
 
     #[Route('/a/{code}', name: 'app_asistencia_alumno')]
-    public function index(string $code,Request $request): Response {
+    public function index(string $code, Request $request): Response
+    {
         $this->session = $request->getSession();
         //$this->session->remove('alumno');
-        
+
         //TODO: usar https://github.com/nayzo/NzoUrlEncryptorBundle para encriptar urls
         $idtomaasistencia = TomaDeAsistencia::urlDecode($code);
 
         if (is_numeric($idtomaasistencia)) {
             $idtomaasistencia = intval($idtomaasistencia);
         } else {
+            $this->session->remove('alumno');
             throw new AccessDeniedHttpException();
         }
 
         $tomaasitencia = $this->cr->find($idtomaasistencia);
-        if (is_null($tomaasitencia) || $tomaasitencia->getEstado() != TomaDeAsistencia::ESTADO_INICIADO) {
+        if (is_null($tomaasitencia)) {
             $this->session->remove('alumno');
             throw new AccessDeniedHttpException();
         }
-        
+
+        if ($tomaasitencia->getEstado() != TomaDeAsistencia::ESTADO_INICIADO) {
+            $this->session->remove('alumno');
+            return $this->redirectToRoute('app_asistencia_alumno_no', ['code' => $code]);
+        }
+
         if (!is_null($this->session->get('alumno', null))) {
-            dump($this->session->get('alumno'));
+            $alumno = $this->session->get('alumno');
+            $asistencia = $this->ar->findOneBy(['alumno' => $alumno, 'tomaDeAsistencia' => $tomaasitencia]);
+            $asistencia->setPresente(true);
+            //$this->em->persist($asistencia);
+            $this->em->flush();
         } else {
             return $this->redirectToRoute('app_login_alumno_asistencia', ['code' => $code]);
         }
         return $this->render('asistencia_alumno/index.html.twig', [
             //'tomaassitencia' => $tomaasitencia,
+        ]);
+    }
+
+    #[Route('/noa/{code}', name: 'app_asistencia_alumno_no')]
+    public function no(string $code): Response
+    {
+        $idtomaasistencia = TomaDeAsistencia::urlDecode($code);
+
+        if (!is_numeric($idtomaasistencia)) {
+            throw new AccessDeniedHttpException();
+        }
+
+        return $this->render('asistencia_alumno/no.html.twig', [
+            'code' => $code,
         ]);
     }
 }
